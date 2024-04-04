1f6a3d7bdce51653c934595140455236ca260019
priyanshu082 <priyanshusingh216@gmail.com>
2024-04-04 16:03:55 +0530

    python backend files are added

---
+++ backEndPython/apis.py
@@ -0,0 +1,236 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+from pymongo import MongoClient
+import requests
+import pandas as pd
+import numpy as np
+
+app = Flask(__name__)
+CORS(app)
+client = MongoClient('mongodb://localhost:27017/')
+db = client['optionChainData']
+
+@app.route('/expirydates', methods=['POST'])
+def expiryDates():
+    try:
+        request_data = request.get_json()
+        symbol = request_data.get('symbol')
+
+        symbolCollection = db[symbol]
+        data = symbolCollection.distinct('Expiry_Date')
+    except:
+        data = "Error"
+    response = {"data": data}
+    return jsonify(response)
+
+@app.route('/strikeprices', methods=['POST'])
+def strikePrices():
+    try:
+        request_data = request.get_json()
+        symbol = request_data.get('symbol')
+        expiryDate = request_data.get('expiryDate')
+        noOfStrikes = int(request_data.get('noOfStrikes'))
+
+        symbolCollection = db[symbol]
+        data = [x for x in symbolCollection.find({'Expiry_Date': expiryDate}, {'_id':0})]
+        allStikePrices = [x['Strike_Price'] for x in data]
+        allStikePrices = set(allStikePrices)
+        allStikePrices = list(allStikePrices)
+        allStikePrices.sort()
+        if len(allStikePrices) > noOfStrikes*2:
+            elementsToLeave = len(allStikePrices)-noOfStrikes*2
+            requiredStrikePrices = allStikePrices[elementsToLeave//2:(-1)*(elementsToLeave//2)] 
+        else:
+            requiredStrikePrices = allStikePrices
+
+        data = requiredStrikePrices
+    except:
+        data = "Error"
+    response = {"data": data}
+    return jsonify(response)
+
+@app.route('/all', methods=['POST'])
+def allData():
+    try:
+        request_data = request.get_json()
+        symbol = request_data.get('symbol')
+        expiryDate = request_data.get('expiryDate')
+        noOfStrikes = int(request_data.get('noOfStrikes'))
+        timeRange = request_data.get('timeRange')
+       
+
+        startTime, endTime = timeRange.split('-')
+        symbolCollection = db[symbol]
+
+        data = [x for x in symbolCollection.find({'Expiry_Date': expiryDate, 'Time': {"$gte": startTime, '$lte': endTime }}, {'_id':0})]
+        allStikePrices = [x['Strike_Price'] for x in data]
+        allStikePrices = set(allStikePrices)
+        allStikePrices = list(allStikePrices)
+        allStikePrices.sort()
+        if len(allStikePrices) > noOfStrikes*2:
+            elementsToLeave = len(allStikePrices)-noOfStrikes*2
+            requiredStrikePrices = allStikePrices[elementsToLeave//2:(-1)*(elementsToLeave//2)] 
+        else:
+            requiredStrikePrices = allStikePrices
+
+        data = [x for x in data if x['Strike_Price'] in requiredStrikePrices]
+        df = pd.DataFrame(data)
+
+        df['S_C_Calls'] = df.groupby('Time')['C_Calls'].transform('sum')
+        df['S_C_Puts'] = df.groupby('Time')['C_Puts'].transform('sum')
+        df['S_COI_Calls'] = df.groupby('Time')['COI_Calls'].transform('sum')
+        df['S_COI_Puts'] = df.groupby('Time')['COI_Puts'].transform('sum')
+        df['R_S_COI'] = np.where(df["S_COI_Calls"] != 0, df['S_COI_Puts'] / df["S_COI_Calls"], np.nan)
+
+        data = df.to_dict(orient='records')
+    except:
+        data = "Error"
+    response = {"data": data}
+    return jsonify(response)
+
+@app.route('/commutativesum', methods=['POST'])
+def CommutativeSumData():
+    try:
+        request_data = request.get_json()
+        symbol = request_data.get('symbol')
+        expiryDate = request_data.get('expiryDate')
+        noOfStrikes = int(request_data.get('noOfStrikes'))
+        timeInterval = int(request_data.get('timeInterval'))#2 4 6 8 10
+
+        symbolCollection = db[symbol]
+        
+        data = [x for x in symbolCollection.find({'Expiry_Date': expiryDate}, {'_id':0})]
+        allStikePrices = [x['Strike_Price'] for x in data]
+        allStikePrices = set(allStikePrices)
+        allStikePrices = list(allStikePrices)
+        allStikePrices.sort()
+        if len(allStikePrices) > noOfStrikes*2:
+            elementsToLeave = len(allStikePrices)-noOfStrikes*2
+            requiredStrikePrices = allStikePrices[elementsToLeave//2:(-1)*(elementsToLeave//2)] 
+        else:
+            requiredStrikePrices = allStikePrices
+
+        data = [x for x in data if x['Strike_Price'] in requiredStrikePrices]
+        df = pd.DataFrame(data)
+
+        df['S_C_Calls'] = df.groupby('Time')['C_Calls'].transform('sum')
+        df['S_C_Puts'] = df.groupby('Time')['C_Puts'].transform('sum')
+        df['S_COI_Calls'] = df.groupby('Time')['COI_Calls'].transform('sum')
+        df['S_COI_Puts'] = df.groupby('Time')['COI_Puts'].transform('sum')
+        df['PC_Calls'] = df['S_C_Calls']/df['S_COI_Calls']
+        df['PC_Puts'] = df['S_C_Puts']/df['S_COI_Puts']
+
+        df = df.sort_values('Time')
+        subDf = df[['Time', 'S_COI_Calls', 'S_C_Calls', 'PC_Calls', 'S_COI_Puts', 'S_C_Puts', 'PC_Puts']]
+        subDf = subDf.drop_duplicates()
+
+        data = subDf.to_dict(orient='records')[::timeInterval//2]
+    except:
+        data = "Error"
+    response = {"data": data}
+    return jsonify(response)
+
+@app.route('/pcr', methods=['POST'])
+def pcrData():
+    try:
+        request_data = request.get_json()
+        symbol = request_data.get('symbol')
+        expiryDate = request_data.get('expiryDate')
+        noOfStrikes = int(request_data.get('noOfStrikes'))
+        symbolCollection = db[symbol]
+
+        data = [x for x in symbolCollection.find({'Expiry_Date': expiryDate}, {'_id':0})]
+        allStikePrices = [x['Strike_Price'] for x in data]
+        allStikePrices = set(allStikePrices)
+        allStikePrices = list(allStikePrices)
+        allStikePrices.sort()
+        if len(allStikePrices) > noOfStrikes*2:
+            elementsToLeave = len(allStikePrices)-noOfStrikes*2
+            requiredStrikePrices = allStikePrices[elementsToLeave//2:(-1)*(elementsToLeave//2)] 
+        else:
+            requiredStrikePrices = allStikePrices
+
+        data = [x for x in data if x['Strike_Price'] in requiredStrikePrices]
+        df = pd.DataFrame(data)
+
+        df['S_C_Calls'] = df.groupby('Time')['C_Calls'].transform('sum')
+        df['S_C_Puts'] = df.groupby('Time')['C_Puts'].transform('sum')
+        df['S_COI_Calls'] = df.groupby('Time')['COI_Calls'].transform('sum')
+        df['S_COI_Puts'] = df.groupby('Time')['COI_Puts'].transform('sum')
+        df['R_S_COI'] = np.where(df["S_COI_Calls"] != 0, df['S_COI_Puts'] / df["S_COI_Calls"], np.nan)
+        df = df[['Time', 'R_S_COI', 'underlyingValue']]
+        data = df.to_dict(orient='records')
+    except:
+        data = "Error"
+    response = {"data": data}
+    return jsonify(response)
+
+@app.route('/price', methods=['POST'])
+def priceData():
+    try:
+        request_data = request.get_json()
+        symbol = request_data.get('symbol')
+
+        symbolCollection = db[symbol]
+        data = [x for x in symbolCollection.find({}, {'_id':0, "Time":1,  'underlyingValue':1})]
+        df = pd.DataFrame(data)
+
+        df  = df.drop_duplicates()
+
+        data = df.to_dict(orient='records')
+    except:
+        data = "Error"
+    response = {"data": data}
+    return jsonify(response)
+
+@app.route('/strikegraph', methods=['POST'])
+def strikeGraph():
+    try:
+        request_data = request.get_json()
+        symbol = request_data.get('symbol')
+        expiryDate = request_data.get('expiryDate')
+        strikePrice = int(request_data.get('strikePrice'))
+        timeInterval = int(request_data.get('timeInterval'))#2 4 6 8 10
+
+        symbolCollection = db[symbol]
+        
+        data = [x for x in symbolCollection.find({'Expiry_Date': expiryDate, 'Strike_Price': strikePrice}, {'_id':0, 'Time':1,'LTP_Calls':1, 'LTP_Puts':1,'underlyingValue':1})]
+        df = pd.DataFrame(data)
+
+        df.sort_values('Time')
+        data = df.to_dict(orient='records')[::timeInterval//2]
+
+    except:
+        data = "Error"
+    response = {"data": data}
+    return jsonify(response) 
+
+@app.route('/buysell', methods=['POST'])
+def buySellData():
+    try:
+        request_data = request.get_json()
+        symbol = request_data.get('symbol')
+        expiryDate = request_data.get('expiryDate')
+        strikePrice = int(request_data.get('strikePrice'))
+        timeInterval = int(request_data.get('timeInterval'))#2 4 6 8 10
+        print(symbol,expiryDate,strikePrice,timeInterval)
+
+        symbolCollection = db[symbol]
+        
+        data = [x for x in symbolCollection.find({'Expiry_Date': expiryDate, 'Strike_Price': strikePrice}, {'_id':0, 'Time':1,'TotalBuyQuantity_Calls':1, 'TotalSellQuantity_Calls':1, 'TotalBuyQuantity_Puts':1, 'TotalSellQuantity_Puts':1})]
+        df = pd.DataFrame(data)
+
+        df.sort_values('Time')
+        data = df.to_dict(orient='records')[::timeInterval//2]
+
+    except:
+        data = "Error"
+    response = {"data": data}
+    return jsonify(response)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
---
+++ backEndPython/main.py
@@ -0,0 +1,235 @@
+import multiprocessing
+import time
+import pandas as pd
+import numpy as np
+import re
+from datetime import datetime
+import pytz
+from pymongo import MongoClient
+from nsepython import nsefetch
+
+
+def call_api(symbol):
+    # MongoDB Connection
+    client = MongoClient('mongodb://localhost:27017/')
+    db = client['optionChainData']
+    
+    if symbol in ["NIFTY", "FINNIFTY", "BANKNIFTY", "MIDCPNIFTY"]:
+        url = f"https://www.nseindia.com/api/option-chain-indices?symbol={symbol}"
+    else:
+        url = f"https://www.nseindia.com/api/option-chain-equities?symbol={symbol}"
+        symbol = re.sub(r'[^a-zA-Z0-9]', '_', symbol) #to avoid special characters in table names
+
+    try:
+        data = nsefetch(url)
+        data = data['records']
+        print(f'Data Fetched: {symbol}')
+    except:
+        return False
+
+    underlyingValue = data['underlyingValue']
+    initialUnderlyingValueCollection = db['initialUnderlyingValues']
+
+    if not initialUnderlyingValueCollection.find_one({'symbol': symbol}):
+        initialUnderlyingValueCollection.insert_one({'symbol': symbol, 'underlyingValue': underlyingValue})
+        initialUnderlyingValue = underlyingValue
+    else:
+        initialUnderlyingValue=initialUnderlyingValueCollection.find_one({'symbol': symbol})['underlyingValue']
+
+
+    timestamp = data['timestamp'][-8:]
+
+    relevant_data = data['data']
+    extracted_data = []
+
+    for i in relevant_data:
+        record = {
+            'Time': timestamp,
+            'underlyingValue': underlyingValue,
+            'Strike_Price': i['strikePrice'],
+            'COI_Calls': 0 if i.get('CE', 0)==0 else i['CE']['changeinOpenInterest'],
+            'COI_Puts': 0 if i.get('PE', 0)==0 else i['PE']['changeinOpenInterest'],
+            'LTP_Calls': 0 if i.get('CE', 0)==0 else i['CE']['lastPrice'],
+            'LTP_Puts': 0 if i.get('PE', 0)==0 else i['PE']['lastPrice'],
+            'TotalBuyQuantity_Calls': 0 if i.get('CE', 0)==0 else i['CE']['totalBuyQuantity'],
+            'TotalSellQuantity_Calls': 0 if i.get('CE', 0)==0 else i['CE']['totalSellQuantity'],
+            'TotalBuyQuantity_Puts': 0 if i.get('PE', 0)==0 else i['PE']['totalBuyQuantity'],
+            'TotalSellQuantity_Puts': 0 if i.get('PE', 0)==0 else i['PE']['totalSellQuantity'],
+            'OpenInterest_Calls': 0 if i.get('CE', 0)==0 else i['CE']['openInterest'],
+            'OpenInterest_Putss': 0 if i.get('PE', 0)==0 else i['PE']['openInterest'],
+            'Expiry_Date': i['expiryDate']
+        }
+        extracted_data.append(record)
+
+    df = pd.DataFrame(extracted_data)
+
+    df['Expiry_Date'] = pd.to_datetime(df['Expiry_Date'], format='%d-%b-%Y')
+    df = df.sort_values(by=['Expiry_Date', 'Strike_Price'])
+
+    above_df = df[df['Strike_Price'] > initialUnderlyingValue].groupby('Expiry_Date').head(20)
+    below_df = df[df['Strike_Price'] <= initialUnderlyingValue].groupby('Expiry_Date').tail(20)
+    result_df = pd.concat([above_df, below_df], ignore_index=True)
+    result_df = result_df.sort_values(by=['Expiry_Date', 'Strike_Price'])
+
+    symbolCollection = db[symbol]
+
+    if symbolCollection.find_one({}):
+        #expected error due to id col in collection
+        past_all_df = pd.DataFrame(symbolCollection.find({}))
+        past_all_df['Expiry_Date'] = pd.to_datetime(past_all_df['Expiry_Date'], format='%Y-%m-%d')
+        prev_df = past_all_df.tail(len(result_df))
+        prev_df = prev_df.sort_values(by=['Expiry_Date', 'Strike_Price'])
+        prev_df.reset_index(drop=True, inplace=True)
+        result_df.reset_index(drop=True, inplace=True)
+
+        result_df['C_Calls'] = result_df['COI_Calls'] - prev_df['COI_Calls']
+        result_df['C_Puts'] = result_df['COI_Puts'] - prev_df['COI_Puts']
+        result_df['CLTP_Calls'] = result_df['LTP_Calls'] - prev_df['LTP_Calls']
+        result_df['CLTP_Puts'] = result_df['LTP_Puts'] - prev_df['LTP_Puts']
+        result_df['C_Amt_Calls_Cr'] = (result_df['C_Calls']*132000)/10000000
+        result_df['C_Amt_Puts_Cr'] = (result_df['C_Puts']*132000)/10000000
+
+        # 1:-Short Covering		If prize is Increasing and OI is decresing.
+        # 2:-Short Buildup		If prize is Decreasing and OI is Increasing.
+        # 3:-Long Buildup		If prize is Increasing and OI is increasing.
+        # 4:-Long Unwinding		If prize is decresing and OI is decresing.
+
+        result_df['Long_Short_Calls'] = np.where((result_df['CLTP_Calls'] < 0) & (result_df['C_Calls'] > 0), 'Short Buildup',
+               np.where((result_df['CLTP_Calls'] < 0) & (result_df['C_Calls'] < 0), 'Long Unwinding',
+               np.where((result_df['CLTP_Calls'] > 0) & (result_df['C_Calls'] < 0), 'Short Covering',
+               np.where((result_df['CLTP_Calls'] > 0) & (result_df['C_Calls'] > 0), 'Long Buildup', ''))))
+        
+        result_df['Long_Short_Puts'] = np.where((result_df['CLTP_Puts'] < 0) & (result_df['C_Puts'] > 0), 'Short Buildup',
+               np.where((result_df['CLTP_Puts'] < 0) & (result_df['C_Puts'] < 0), 'Long Unwinding',
+               np.where((result_df['CLTP_Puts'] > 0) & (result_df['C_Puts'] < 0), 'Short Covering',
+               np.where((result_df['CLTP_Puts'] > 0) & (result_df['C_Puts'] > 0), 'Long Buildup', ''))))
+
+        # if result_df['CLTP_Calls'] > 0 and result_df['C_Calls'] < 0:
+        #     result_df['Long_Short_Calls'] = 'Short Covering' 
+        # elif result_df['CLTP_Calls'] < 0 and result_df['C_Calls'] > 0:
+        #     result_df['Long_Short_Calls'] = 'Short Buildup' 
+        # elif result_df['CLTP_Calls'] > 0 and result_df['C_Calls'] > 0:
+        #     result_df['Long_Short_Calls'] = 'Long Buildup' 
+        # elif result_df['CLTP_Calls'] < 0 and result_df['C_Calls'] < 0:
+        #     result_df['Long_Short_Calls'] = 'Long Unwinding' 
+
+        # if result_df['CLTP_Puts'] > 0 and result_df['C_Puts'] < 0:
+        #     result_df['Long_Short_Puts'] = 'Short Covering' 
+        # elif result_df['CLTP_Puts'] < 0 and result_df['C_Puts'] > 0:
+        #     result_df['Long_Short_Puts'] = 'Short Buildup' 
+        # elif result_df['CLTP_Puts'] > 0 and result_df['C_Puts'] > 0:
+        #     result_df['Long_Short_Puts'] = 'Long Buildup' 
+        # elif result_df['CLTP_Puts'] < 0 and result_df['C_Puts'] < 0:
+        #     result_df['Long_Short_Puts'] = 'Long Unwinding' 
+
+    else:
+        result_df['C_Calls'] = 0
+        result_df['C_Puts'] = 0
+        result_df['CLTP_Calls'] = 0
+        result_df['CLTP_Puts'] = 0
+        result_df['C_Amt_Calls_Cr'] = 0.0
+        result_df['C_Amt_Puts_Cr'] = 0.0
+        result_df['Long_Short_Calls'] = None
+        result_df['Long_Short_Puts'] = None
+
+    result_df['Expiry_Date'] = result_df['Expiry_Date'].dt.strftime('%Y-%m-%d')
+
+    #there is a bug that is the data of the first cycle will be wrong as it will perform the calc based on the data of last cycle of prev day.
+    #it is happening as table is never empty or are nver deleted hence the above except condition will never run except the first time the database is created.
+
+    if timestamp < '09:17:00':
+        #to delete data of previous day
+        symbolCollection.delete_many({})
+        symbolCollection.insert_many(result_df.to_dict(orient='records'))
+    else:
+        symbolCollection.insert_many(result_df.to_dict(orient='records'))
+
+    return True
+
+        
+def call_batch(symbols):
+    results = []
+    for symbol in symbols:
+        results.append(call_api(symbol))
+    return results
+
+
+def market_status():
+    data=nsefetch('https://www.nseindia.com/api/option-chain-indices?symbol=NIFTY')
+    data=data['records']
+    timestamp = data['timestamp'][-8:]
+
+    if timestamp[0:6] == '15:30':
+        return False
+    else:
+        return True
+
+        
+
+if __name__ == "__main__":
+    # IST = pytz.timezone('Asia/Kolkata') 
+    # while True:
+    #     if not ('09:18:00' <= datetime.now(IST).strftime("%H:%M:%S") <= '15:33:00'):
+    #         continue
+    #     else:
+    #         if market_status():
+    #             while ('09:18:00' <= datetime.now(IST).strftime("%H:%M:%S") <= '15:33:00'):
+    #                         start = time.time()
+
+    #                         with open("backend/symbols.txt") as f:
+    #                             data = f.readlines()
+
+    #                         num_cores = multiprocessing.cpu_count()
+    #                         symbols = [i.strip('\n') for i in data]
+    #                         no_of_batches = 3*num_cores
+    #                         avg_elements = len(symbols) // no_of_batches
+    #                         remainder = len(symbols) % no_of_batches
+
+    #                         symbol_batches = [symbols[i * avg_elements + min(i, remainder):(i + 1) * avg_elements + min(i + 1, remainder)] for i in range(no_of_batches)]       
+
+
+    #                         # Use the number of CPU cores as the number of processes
+    #                         with multiprocessing.Pool(processes=no_of_batches) as pool:
+    #                             # Use the pool to map the worker function onto the tasks
+    #                             results = pool.map(call_batch, symbol_batches)
+
+    #                         flattened_list = [item for sublist in results for item in sublist]
+
+    #                         print(f"\nPassed: {flattened_list.count(True)}\nFailed: {flattened_list.count(False)}")
+    #                         end = time.time()
+
+    #                         print(f'{end-start}\n')
+    #                         time.sleep(120-(end-start))
+            
+    #         else:
+    #             #to sleep on holiday for the hours in which market remains open
+    #             time.sleep(25200)
+
+
+
+        with open("symbols.txt") as f:
+            data = f.readlines()
+
+        symbols = [i.strip('\n') for i in data]
+        num_cores = multiprocessing.cpu_count()
+        no_of_batches = 3*num_cores
+        avg_elements = len(symbols) // no_of_batches
+        remainder = len(symbols) % no_of_batches
+
+        symbol_batches = [symbols[i * avg_elements + min(i, remainder):(i + 1) * avg_elements + min(i + 1, remainder)] for i in range(no_of_batches)]       
+        
+        while True:
+            start = time.time()
+
+            # Use the number of CPU cores as the number of processes
+            with multiprocessing.Pool(processes=no_of_batches) as pool:
+                # Use the pool to map the worker function onto the tasks
+                results = pool.map(call_batch, symbol_batches)
+
+            flattened_list = [item for sublist in results for item in sublist]
+
+            print(f"\nPassed: {flattened_list.count(True)}\nFailed: {flattened_list.count(False)}")
+            end = time.time()
+
+            print(f'{end-start}\n')
+            time.sleep(120-(end-start))
